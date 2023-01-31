72df16072d880324dc04311b7dfb22447c0347cc
SIA <anastasia130499@gmail.com>
2023-01-31 18:07:55 +0200

    initial commit

---
+++ cv_flask_app/change_mode.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+def get_time():
+    t = datetime.now().hour
+    if 6 < t < 18:
+        return 'light'
+    else:
+        return 'dark'
---
+++ cv_flask_app/users.py
@@ -0,0 +1,33 @@
+users = [
+    {
+    'mail':'some_mail@gmail.com',
+    'password':'letmeinletmeinletmein',
+    'full_name':'Dyomshina Anastasiia Mikolayvna',
+    'birthdate':'13/04/1999',
+    'tel':'+380843669436',
+    'education':"Level-up - Front End course; IT-Step - Python course",
+    'skills':'HTML/CSS, JS, Python, SQLite, SQLAlchemy, Flask',
+    'experience':'None',
+    'u_img':'cool.jpg'
+    },
+    {
+    'mail':'another_mail@gmail.com',
+    'password':'12345',
+    'full_name':'Smith John',
+    'birthdate':'14/05/2001',
+    'tel':'+380987654321',
+    'education':"Some school or other",
+    'skills':'Java, PHP, C#, C++',
+    'experience':'Some',
+    },
+    {
+    'mail':'cool_mail@gmail.com',
+    'password':'69420',
+    'full_name':'Yamada Hanako',
+    'birthdate':'12/03/1998',
+    'tel':'+380123456789',
+    'education':"good one",
+    'skills':'malbolge, chicken, brainfuck, piet, JavaScript',
+    'experience':'Lots',
+    },
+]
